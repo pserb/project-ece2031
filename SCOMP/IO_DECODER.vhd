dae3bb468218e257e0a45ddd1984c2f4303108e5
pserb <paulserbanescu3@gmail.com>
2024-11-02 23:38:23 -0400

    external memory

--- SCOMP/IO_DECODER.vhd
+++ SCOMP/IO_DECODER.vhd
@@ -10,14 +10,15 @@ ENTITY IO_DECODER IS
 
   PORT
   (
-    IO_ADDR       : IN STD_LOGIC_VECTOR(10 downto 0);
-    IO_CYCLE      : IN STD_LOGIC;
-    SWITCH_EN     : OUT STD_LOGIC;
-    LED_EN        : OUT STD_LOGIC;
-    TIMER_EN      : OUT STD_LOGIC;
-    HEX0_EN       : OUT STD_LOGIC;
-    HEX1_EN       : OUT STD_LOGIC;
-    EXTMEM_EN     : OUT STD_LOGIC
+    IO_ADDR         : IN STD_LOGIC_VECTOR(10 downto 0);
+    IO_CYCLE        : IN STD_LOGIC;
+    SWITCH_EN       : OUT STD_LOGIC;
+    LED_EN          : OUT STD_LOGIC;
+    TIMER_EN        : OUT STD_LOGIC;
+    HEX0_EN         : OUT STD_LOGIC;
+    HEX1_EN         : OUT STD_LOGIC;
+    EXTMEMADDR_EN   : OUT STD_LOGIC;
+    EXTMEMDATA_EN   : OUT STD_LOGIC
   );
 
 END ENTITY;
@@ -30,11 +31,12 @@ begin
 
   ADDR_INT <= TO_INTEGER(UNSIGNED(IO_ADDR));
         
-  SWITCH_EN    <= '1' WHEN (ADDR_INT = 16#000#) and (IO_CYCLE = '1') ELSE '0';
-  LED_EN       <= '1' WHEN (ADDR_INT = 16#001#) and (IO_CYCLE = '1') ELSE '0';
-  TIMER_EN     <= '1' WHEN (ADDR_INT = 16#002#) and (IO_CYCLE = '1') ELSE '0';
-  HEX0_EN      <= '1' WHEN (ADDR_INT = 16#004#) and (IO_CYCLE = '1') ELSE '0';
-  HEX1_EN      <= '1' WHEN (ADDR_INT = 16#005#) and (IO_CYCLE = '1') ELSE '0';
-  EXTMEM_EN    <= '1' WHEN (ADDR_INT = 16#070#) and (IO_CYCLE = '1') ELSE '0';
+  SWITCH_EN     <= '1' WHEN (ADDR_INT = 16#000#) and (IO_CYCLE = '1') ELSE '0';
+  LED_EN        <= '1' WHEN (ADDR_INT = 16#001#) and (IO_CYCLE = '1') ELSE '0';
+  TIMER_EN      <= '1' WHEN (ADDR_INT = 16#002#) and (IO_CYCLE = '1') ELSE '0';
+  HEX0_EN       <= '1' WHEN (ADDR_INT = 16#004#) and (IO_CYCLE = '1') ELSE '0';
+  HEX1_EN       <= '1' WHEN (ADDR_INT = 16#005#) and (IO_CYCLE = '1') ELSE '0';
+  EXTMEMADDR_EN <= '1' WHEN (ADDR_INT = 16#070#) and (IO_CYCLE = '1') ELSE '0';
+  EXTMEMDATA_EN <= '1' WHEN (ADDR_INT = 16#071#) and (IO_CYCLE = '1') ELSE '0';
       
 END a;
